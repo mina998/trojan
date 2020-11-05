4c350e159a8b0c9a5c00042b910157de627add27
mina998 <iosss@qq.com>
2020-11-05 11:50:38 +0800

    修正

--- trojan.sh
+++ trojan.sh
@@ -20,14 +20,15 @@ fi
 source ~/.bashrc
 
 echo ${Blue}
-read -p "请输入域名(ss.demo.com):" domain
+read -p "请输入域名(eg:ss.demo.com):" domain
 echo ${Font}
-
+# 获取本机IP
 localh_ip=$(curl https://api-ipv4.ip.sb/ip)
+# 获取域名解析IP
 domain_ip=$(ping "${domain}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
-
+#
 echo "${Green}域名dns解析IP${Font}: ${domain_ip}"
-
+#
 if [ "$localh_ip" = "$domain_ip" ]; then
 	echo "${Green}域名解析成功! ${Font}"
 else
@@ -42,7 +43,7 @@ else
 			;;	
 	esac
 fi
-
+#
 echo "${Blue}开始申请证书.${Font}"
 ~/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force
 #
@@ -52,7 +53,8 @@ mkdir ~/ssl
 
 echo "${Blue}证书安装中. 请稍等...${Font}"
 sleep 3
-
+#
+echo "${Blue}安装trojan程序${Font}"
 # 下载trojan-gfw
 wget https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
 # 解压缩
@@ -129,6 +131,8 @@ systemctl daemon-reload
 systemctl start trojan
 systemctl enable trojan
 
+#
+echo "${Blue}安装caddy程序${Font}"
 # 下载Caddy
 wget https://github.com/caddyserver/caddy/releases/download/v2.2.1/caddy_2.2.1_linux_amd64.deb
 # 安装Caddy
@@ -148,11 +152,11 @@ EOF
 # 重启Caddy
 caddy stop && caddy start && cd ~
 
+echo "${Green}查看状态${Font}: ${Blue}systemctl status trojan${Font}"
+echo "${Green}停　　止${Font}: ${Blue}systemctl stop trojan${Font}"
+echo "${Green}启　　动${Font}: ${Blue}systemctl restart trojan${Font}"
+
 echo "${Blue}服务器地址${Font}: ${Red}${domain}${Font}"
 echo "${Blue}端　　　口${Font}: ${Red}443${Font}"
 echo "${Blue}密　　　码${Font}: ${Red}${password}${Font}"
 echo "${Blue}传输层加密${Font}: ${Red}tls${Font}"
-
-echo "${Green}查看状态${Font}: ${Blue}systemctl status trojan${Font}"
-echo "${Green}停　　止${Font}: ${Blue}systemctl stop trojan${Font}"
-echo "${Green}启　　动${Font}: ${Blue}systemctl restart trojan${Font}"
